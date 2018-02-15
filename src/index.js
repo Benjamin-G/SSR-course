0411352851e17e8ec141578ab67077da29e7b093
Benjamin Geyer <brgeyer49@gmail.com>
2018-02-14 19:05:19 -0500

    refactored server and created renderer function
    also optmized webpack for the server

--- src/index.js
+++ src/index.js
@@ -3,31 +3,15 @@
 // const renderToString = require('react-dom/server').renderToString
 // const Home = require('./client/components/Home').default
 // ^^^ We do not need this because of webpack/babel
-
 import express from 'express'
-import React from 'react'
-import { renderToString } from 'react-dom/server'
-import Home from './client/components/Home'
+import renderer from './helpers/renderer'
 
 const app = express()
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  const content = renderToString(<Home />)
-
-  const html = `
-    <html>
-      <head>
-        <body>
-          <div id="root">${content}</div>
-          <script src="bundle.js"></script>
-        </body>
-      </head>
-    </html>
-  `
-
-  res.send(html)
+  res.send(renderer())
 })
 
 app.listen(3000, () => {
