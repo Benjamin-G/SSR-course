fc3f6932a7ed7b76f4df77339969bc2ceb132fba
Benjamin Geyer <brgeyer49@gmail.com>
2018-03-12 09:39:35 -0400

    Client Side renderer setup

--- src/index.js
+++ src/index.js
@@ -6,12 +6,21 @@
 import 'babel-polyfill'
 import express from 'express'
 import { matchRoutes } from 'react-router-config'
+import proxy from 'express-http-proxy'
 
 import Routes from './client/Routes'
 import renderer from './helpers/renderer'
 import createStore from './helpers/createStore'
 
 const app = express()
+
+//on all calls to "/api/..." => proxy to handle cookie for Oauth
+app.use('/api', proxy('http://react-ssr-api.herokuapp.com', {
+  proxyReqOptDecorator(opts) {
+    opts.header['x-forwarded-host'] = 'localhost:3000'
+    return opts
+  }
+}))
 
 app.use(express.static('public'))
 
